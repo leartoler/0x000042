d0ba5ac3fd2ef0370ec74c77b6d9cfb789bbf9cb
leartoler <fabiangg@protonmail.ch>
2026-01-17 02:17:21 -0600

    portfolio 04

--- public/gallery.js
+++ public/gallery.js
@@ -1,2 +1,6 @@
-lightGallery(document.getElementById('lightgallery'))
-
+lightGallery(document.getElementById("lightgallery"), {
+  plugins: [lgVideo, lgZoom, lgFullscreen],
+  speed: 500,
+  download: false,
+  videojs: false
+});
